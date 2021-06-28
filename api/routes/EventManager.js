7f08b6f820a2ed2997d3dd6365e8f12f7f2ca9a2
realakshay <gaikwadakshay663@gmail.com>
2021-06-28 14:15:27 +0530

    update event manager details

--- api/routes/EventManager.js
+++ api/routes/EventManager.js
@@ -19,8 +19,34 @@ router.put("/changePwd", verify, async(req, res) =>{
         }
     }catch(err){
         res.status(400).send({message: err})
+    }  
+})
+
+router.put("/update", verify, async(req, res) =>{
+    try{
+        const eventManager = await EventManager.findById(req.user._id);
+        if(req.body.companyName) eventManager.companyName = req.body.companyName;
+        if(req.body.ownerFirstName) eventManager.ownerFirstName = req.body.ownerFirstName;
+        if(req.body.ownerLastName) eventManager.ownerLastName = req.body.ownerLastName;
+        if(req.body.email) eventManager.email = req.body.email;
+        if(req.body.phoneNumber) eventManager.phoneNumber = req.body.phoneNumber;
+        if(req.body.whatsappBusinessNumber) eventManager.whatsappBusinessNumber = req.body.whatsappBusinessNumber;
+        if(req.body.fbPageUrl) eventManager.fbPageUrl = req.body.fbPageUrl;
+        if(req.body.instagramPageUrl) eventManager.instagramPageUrl = req.body.compainstagramPageUrlnyName;
+        if(req.body.linkedinPageUrl) eventManager.linkedinPageUrl = req.body.linkedinPageUrl;
+        if(req.body.twitterPageUrl) eventManager.twitterPageUrl = req.body.twitterPageUrl;
+        if(req.body.youtubePageUrl) eventManager.youtubePageUrl = req.body.youtubePageUrl;
+        if(req.body.city) eventManager.city = req.body.city;
+        if(req.body.state) eventManager.state = req.body.state;
+        if(req.body.country) eventManager.country = req.body.country;
+        if(req.body.pinCode) eventManager.pinCode = req.body.pinCode;
+        if(req.body.address) eventManager.address = req.body.address;
+
+        await eventManager.save()
+        res.status(200).send({message: "updated"})
+    }catch(err){
+        res.status(400).send({message: err})
     }
-    
 })
 
 module.exports = router;
