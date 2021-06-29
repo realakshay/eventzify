5292da87f592f861e7489d0babd875d35e6b31b9
realakshay <gaikwadakshay663@gmail.com>
2021-06-29 13:07:41 +0530

    update quotation

--- api/routes/OrderRoute.js
+++ api/routes/OrderRoute.js
@@ -62,4 +62,43 @@ router.get("/all", verify, async (req, res) => {
   }
 });
 
+router.put("/update", verify, async (req, res) => {
+  if (!req.body.orderId)
+    return res.send(400).send({ message: "couldn't found order id" });
+
+  let date = new Date(req.body.eventDate);
+  let mm =
+    date.getMonth() < 10 ? "0" + `${date.getMonth() + 1}` : date.getMonth() + 1;
+  let dd = date.getDate() < 10 ? "0" + `${date.getDate()}` : date.getDate();
+  let finalDate = date.getFullYear() + "/" + mm + "/" + dd;
+
+  try {
+    const order = await Order.findById(req.body.orderId);
+    if (req.body.numberOfAttendees)
+      order.numberOfAttendees = req.body.numberOfAttendees;
+    if (req.body.selectedEvent) order.selectedEvent = req.body.selectedEvent;
+    if (req.body.eventDate) order.eventDate = finalDate;
+    if (req.body.eventTime) order.eventTime = req.body.eventTime;
+    if (req.body.eventVenue) order.eventVenue = req.body.eventVenue;
+    if (req.body.subEvents) order.subEvents = req.body.subEvents;
+    if (req.body.starterData) order.starterData = req.body.starterData;
+    if (req.body.saladData) order.saladData = req.body.saladData;
+    if (req.body.soupData) order.soupData = req.body.soupData;
+    if (req.body.dessertData) order.dessertData = req.body.dessertData;
+    if (req.body.mainCourseData) order.mainCourseData = req.body.mainCourseData;
+    if (req.body.soupCount) order.soupCount = req.body.soupCount;
+    if (req.body.saladCount) order.saladCount = req.body.saladCount;
+    if (req.body.starterCount) order.starterCount = req.body.starterCount;
+    if (req.body.mainCourseCount)
+      order.mainCourseCount = req.body.mainCourseCount;
+    if (req.body.totalPrice) order.totalPrice = req.body.totalPrice;
+    if (req.body.title) order.title = req.body.title;
+    if (req.body.messageText) order.messageText = req.body.messageText;
+
+    await order.save();
+    res.status(200).send({ message: "order updated" });
+  } catch (err) {
+    res.status(400).send({ message: err });
+  }
+});
 module.exports = router;
